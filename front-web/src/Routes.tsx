efcd370642ef72f84937a1b1fbcaca1530ee491b
Cristiano Pontes <pontescr@gmail.com>
2021-01-10 10:45:51 -0300

    Search order location

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom"
-import { resourceLimits } from "worker_threads"
 import Home from "./Home"
 import Navbar from "./Navbar"
 import Orders from "./Orders"
